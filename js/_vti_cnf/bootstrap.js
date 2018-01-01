d26dce28c0b34b84af1474d3e6b1cdb62e35583f
Biao Ma <mabiao7288@gmail.com>
2018-01-01 12:00:02 -0500

    Add files via upload

---
+++ js/_vti_cnf/bootstrap.js
@@ -0,0 +1,11 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|18 Mar 2017 17:16:54 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|Bid/2ds.html Bid/controller.html Bid/keyboard.html Bid/maintenance.html Bid/mbook.html Bid/aboutus.html Bid/charger.html Bid/iphone.html Bid/advertise.html Bid/dnd.html Bid/nerf.html Bid/razermouse.html
+vti_author:SR|wargnerma\\ten
+vti_modifiedby:SR|wargnerma\\ten
+vti_timecreated:TR|18 Mar 2017 17:16:54 -0000
+vti_cacheddtm:TX|18 Mar 2017 17:16:54 -0000
+vti_filesize:IR|72084
+vti_syncwith_146.111.120.96\:443/fa17/et_710_m2/mbiao78:TR|18 Mar 2017 17:16:54 -0000
+vti_syncofs_146.111.120.96\:443/fa17/et_710_m2/mbiao78:TW|19 Dec 2017 22:10:06 -0000
